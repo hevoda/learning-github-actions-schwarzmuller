b90e217a26c9dfbd6b1ced3ff08fbcb6623c4fb6
hevoda <herve.evoda@outlook.com>
2026-02-28 17:49:21 +0100

    fix HelpBox.jsx

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -1,12 +1,18 @@
 import './HelpBox.css';
 
-function HelpBox({ title, text }) {
-  return (
-    <article className="help-box">
-      <h2>{title}</h2>
-      <p>{text}</p>
-    </article>
-  );
+function HelpBox({title, text}) {
+    return (
+        <article className="help-box">
+            <h2>{title}</h2>
+            <p>{text}</p>
+        </article>
+    );
 }
 
 export default HelpBox;
+
+
+HelpBox.propTypes = {
+    title: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired
+};
